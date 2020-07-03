0ce4a782c8726ea5f3bf72948c57fa90e921d0ee
elumixor <vladogim97@gmaill.com>
2020-07-03 11:37:28 +0200

    Example HTML mail send with nodemailer and handlebars

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs')
+const hbs = require('nodemailer-express-handlebars')
 
 const productsPath = './products.json'
 const boundsPath = './bounds/bounds.json'
@@ -54,3 +55,49 @@ app.post('/order', function (request, response) {
 });
 
 app.listen(PORT)
+
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "8e6eb905a0edaf",
+        pass: "6c5399dec35028"
+    }
+});
+
+const handlebarOptions = {
+    viewEngine: {
+        extName: '.hbs',
+        partialsDir: './src/server/views',
+        layoutsDir: './src/server/views',
+        defaultLayout: 'index.hbs',
+    },
+    viewPath: './src/server/views',
+    extName: '.hbs',
+};
+
+transporter.use('compile', hbs(handlebarOptions));
+
+// Step 3
+let mailOptions = {
+    from: 'tabbnabbers@gmail.com', // TODO: email sender
+    to: 'deltamavericks@gmail.com', // TODO: email receiver
+    subject: 'Nodemailer - Test',
+    text: 'Wooohooo it works!!',
+    template: 'index',
+    attachments: [
+        {filename: 'logo.jpg', path: './images/logo.jpg'}
+    ],
+    context: {
+        name: 'Accime Esterling'
+    } // send extra values to template
+};
+
+// Step 4
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log('Email sent!!!');
+});
